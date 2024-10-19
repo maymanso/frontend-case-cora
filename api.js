c7bedbe04e97f88784871ff861e455d7d644bc2b
maymanso <mayaramanso.devel@gmail.com>
2024-10-19 13:04:55 -0300

    fix: fix cors issue

--- api.js
+++ api.js
@@ -2,10 +2,12 @@ const express = require("express");
 const db = require("./db/transactions.json");
 const user = require("./db/user.json");
 const bodyParser = require("body-parser");
+const cors = require('cors');
 
 const port = 3000;
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 
 app.get("/health-check", (_, res) => {
