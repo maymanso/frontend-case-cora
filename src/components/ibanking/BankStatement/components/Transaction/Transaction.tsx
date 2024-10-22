7b35e2902fc26cebca7cda67aeb60506947aefdf
maymanso <mayaramanso.devel@gmail.com>
2024-10-22 12:31:07 -0300

    feat: create component Transaction component responsable to show the line transaction with icon, title, description, date, hour and amount

---
+++ src/components/ibanking/BankStatement/components/Transaction/Transaction.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { formattedDateAndTime } from "../../../../../utils/date.util";
+
+interface TransactionProps {
+  type: 'credit' | 'refund' | 'debit';
+  title: string;
+  description: string;
+  date: string;
+  amount: string;
+  icon: string
+}
+const Transaction: React.FC<TransactionProps> = ({ type, title, date, amount, description, icon }) => {
+  const amountType = {
+    credit: '+',
+    refund: '',
+    debit: '-'
+  };
+
+  return (
+    <article className='bankStatement__grid'>
+      <article className='bankStatement__article'>
+        <img src={icon} />
+        <p className={`bankStatement__title--${type}`}>
+          {title}
+        </p>
+      </article>
+      <p className='bankStatement__cell'>
+        {description}
+      </p>
+      <p className='bankStatement__cell'>
+        {formattedDateAndTime(date)}
+      </p>
+      <p className={`bankStatement__amount--${type}`}>
+        {`${amountType[type]} R$ ${amount}`}
+      </p>
+    </article>
+  );
+};
+
+export default Transaction;
