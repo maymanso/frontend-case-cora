be679d33c44f727cb96394e3b8a6f625614f169c
maymanso <mayaramanso.devel@gmail.com>
2024-10-22 12:57:40 -0300

    feat: update routes with new api routes

--- src/service/auth.service.ts
+++ src/service/auth.service.ts
@@ -1,9 +1,9 @@
-import { DataLoginType } from '../types/auth.type';
+import { DataLoginType } from '../types/auth/auth.type';
 import AxiosService from './axios.service';
 
 export default class AuthService extends AxiosService {
   login = async (data: DataLoginType) => {
-    const response = await this.api.post('/auth', data);
+    const response = await this.api.post('/login', data);
     return response;
   }
 }
