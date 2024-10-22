84c701574ef147c85bf51c79109914ade593f928
maymanso <mayaramanso.devel@gmail.com>
2024-10-22 18:50:53 -0300

    feat: add header component

--- src/routes/private.routes.tsx
+++ src/routes/private.routes.tsx
@@ -1,11 +1,16 @@
 import { Outlet, Navigate } from "react-router-dom";
 import Cookie from "js-cookie";
+import Header from "../components/common/Header/Header";
 const PrivateRoutes = () => {
   const token = Cookie.get('token')
   return (
     token ?
       (
-        <Outlet context={token} />
+        <>
+          <Header isLogin={!!token}>
+            <Outlet context={token} />
+          </Header>
+        </>
       ) :
       <Navigate to="/login" />
   )
