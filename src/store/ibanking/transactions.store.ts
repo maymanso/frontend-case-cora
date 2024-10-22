5b9556f2fe7ef68c93d8ab59706c80365274d709
maymanso <mayaramanso.devel@gmail.com>
2024-10-22 12:26:43 -0300

    feat: create a ibanking store with transaction state and getTransactions dunction

---
+++ src/store/ibanking/transactions.store.ts
@@ -0,0 +1,24 @@
+import { create } from 'zustand';
+
+import IBankingService from '../../service/ibanking.service';
+import { TransacionStoreType } from '../../types/ibanking/transaction.type';
+
+const ibankingService = new IBankingService()
+
+const useTransactionsStore = create<TransacionStoreType>((set) => ({
+  transactions: {
+    results: [],
+    itemsTotal: 0,
+    initialBankBalance: 0
+  },
+  getTransactions: async () => {
+    try {
+      const response = await ibankingService.getTransactions();
+      set({ transactions: response.data })
+    } catch (error) {
+      console.log({ error })
+    }
+  }
+}));
+
+export default useTransactionsStore;
