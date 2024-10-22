9be8cbb5023399e64457c9a4a75cc6b14a1608fc
maymanso <mayaramanso.devel@gmail.com>
2024-10-22 14:50:53 -0300

    fix: rebase with create-transitions-page

--- src/service/ibanking.service.ts
+++ src/service/ibanking.service.ts
@@ -3,7 +3,7 @@ import AxiosService from "./axios.service";
 
 export default class IBankingService extends AxiosService {
   getTransactions = async () => {
-    const response = await this.api('/transactions/bankBalance');
+    const response = await this.api('/transactions/bank-balance');
     return response;
   }
 }
